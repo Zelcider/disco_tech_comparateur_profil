be580942c406c9522063f76a0aa2d899da4b8017
Paul Roman <roman.paul.pro@gmail.com>
2022-07-06 10:59:48 +0200

    feat: Font & theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FlamingoThemeProvider, theme } from "@epsor/flamingo";
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => <FlamingoThemeProvider theme={theme}><Component {...pageProps} /></FlamingoThemeProvider>
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Button } from "@epsor/flamingo";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => (
@@ -48,6 +49,7 @@ const Home: NextPage = () => (
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
+            <Button>yo</Button>
           </a>
         </div>
       </main>
